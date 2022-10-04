87bf817443714e3aa9347a5eb748d9b82e1ae529
35596035 <william82733791@gmail.com>
2022-10-04 21:13:28 +0800

    f

--- 111-1PC5/Test.aspx.cs
+++ 111-1PC5/Test.aspx.cs
@@ -26,39 +26,87 @@ namespace _111_1PC5
                 { 2, 5, 6},
             };
 
-            mt_GetMost(ia_2DArr, ia_Money);
+            int[,] ia_3DArr = new int[8, 3]
+            {
+                { 0, 1, 2},
+                { 3, 4, 5},
+                { 6, 7, 8},
+                { 0, 3, 6},
+                { 1, 4, 7},
+                { 2, 5, 8},
+                { 0, 4, 8},
+                { 2, 4, 6}
+            };
+
+            //mt_GetMost(ia_2DArr, ia_Money);
             Response.Write(mt_GetMost(ia_2DArr, ia_Money));
         }
-
         int mt_GetMost(int[,] ia_2DArr, int[] ia_Money)
         {
             int i_MaxMoney = 0;
             int i_MaxSum = 0;
-            for (int i_Row = 0; i_Row < ia_2DArr.GetLength(0);i_Row++)
+            for (int i_Row = 0; i_Row < ia_2DArr.GetLength(0); i_Row++)
             //GetLength(0)取裡面的第一層長度:3
             //GetLength(1)取裡面的第二層長度:3
             {
-                //3Col
+                //V 3 Col
                 int i_Sum = 0;
                 int i_TmpMoney = 0;
-                //int a = ia_2DArr.GetLength(0);
-                //Response.Write(a + "<br />");
                 for (int i_Col = 0; i_Col < ia_2DArr.GetLength(1);i_Col++)
                 {
                     i_Sum += ia_2DArr[i_Row,i_Col];
                 }
-                int a = i_Sum;
-                Response.Write(a);
-                i_TmpMoney = ia_Money[(i_Sum - 6)];
-                if(i_TmpMoney > i_MaxMoney) {
-                    i_MaxSum = i_Sum;
-                }
-                
-                //3 Row
 
-                //2 incline
+                i_TmpMoney = ia_Money[(i_Sum - 6)];
+                if (i_TmpMoney > i_MaxMoney) {
+                    i_MaxSum = i_Sum;
+                    i_MaxMoney = i_TmpMoney;
+                }
+                i_Sum = 0;
+                //int c = i_Sum;
+                //Response.Write(c + "<br />");
+
+                //V 3 Row
+                for (int i_Col = 0; i_Col < ia_2DArr.GetLength(1); i_Col++)
+                {
+                    i_Sum += ia_2DArr[i_Col, i_Row];
+                }
+
+                i_TmpMoney = ia_Money[(i_Sum - 6)];
+                if (i_TmpMoney > i_MaxMoney)
+                {
+                    i_MaxSum = i_Sum;
+                    i_MaxMoney = i_TmpMoney;
+                }
             }
             return i_MaxSum;
+            
+            //2 incline
+            //00 11 22
+            /*      
+            int i_Sum = 0;
+            int i_TmpMoney = 0;
+            //right incline
+            for (int i_Row = 0; i_Row < ia_2DArr.GetLength(0); i_Row++) 
+            {
+                for (int i_Col = i_Row; i_Col < i_Row+1; i_Col++)
+                {
+                    //Response.Write(i_Row + "<br />");
+                    //Response.Write(i_Col + "<br />");
+                    i_Sum += ia_2DArr[i_Row, i_Col];
+                    Response.Write(i_Sum +"<br />");
+                }
+                if (i_Row >= 2)
+                {
+                    i_TmpMoney = ia_Money[(i_Sum - 6)];
+                    if (i_TmpMoney > i_MaxMoney)
+                    {
+                        i_MaxSum = i_Sum;
+                        i_MaxMoney = i_TmpMoney;
+                    }
+                }
+            }
+            */
         }
     }
 }
